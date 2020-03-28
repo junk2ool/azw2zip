073830434fe3ae3ff53dafb092862b0a5f0195dd
junk2ool <junk2ool@outlook.jp>
2020-03-28 22:03:07 +0900

    fix Linux Python 3

--- DeDRM_Plugin/argv_utils.py
+++ DeDRM_Plugin/argv_utils.py
@@ -46,8 +46,10 @@ def unicode_argv():
         argvencoding = sys.stdin.encoding
         if argvencoding == None:
             argvencoding = "utf-8"
-        return [arg if (type(arg) == unicode) else unicode(arg,argvencoding) for arg in sys.argv]
-
+        if PY2:
+            return [arg if (type(arg) == unicode) else unicode(arg,argvencoding) for arg in sys.argv]
+        else:
+            return sys.argv
 
 def add_cp65001_codec():
   try:
